112d97e44d6a76c4ce90075f2841d71b08d0e7d2
Szymon Kieloch <s.kieloch@selleo.com>
2016-03-06 17:31:49 +0100

    Sort out loading localStorage provider
    Add es7-shim to the app

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,35 +1,30 @@
 /// <reference path="../typings/main.d.ts"/>
 
+import 'es6-shim';
+import 'es7-shim';
 import {enableProdMode, provide} from "angular2/core";
 import {bootstrap, ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS, HashLocationStrategy, LocationStrategy} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
+import * as services from './app/services/services';
 
 const ENV_PROVIDERS = [];
-// depending on the env mode, enable prod mode or add debugging modules
+
 if (process.env.ENV === 'prod') {
   enableProdMode();
 } else {
   ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
 }
 
-/*
- * App Component
- * our top level component that holds all of our components
- */
 import {App} from './app/app';
 
-/*
- * Bootstrap our Angular app with a top level component `App` and inject
- * our Services and Providers into Angular's dependency injection
- */
 document.addEventListener('DOMContentLoaded', function main() {
   return bootstrap(App, [
-    // These are dependencies of our App
     ...HTTP_PROVIDERS,
     ...ROUTER_PROVIDERS,
     ...ENV_PROVIDERS,
-    provide(LocationStrategy, {useClass: HashLocationStrategy}) // use #/ routes, remove this for HTML5 mode
+    provide(LocationStrategy, {useClass: HashLocationStrategy}),
+    Object.values(services)
   ])
   .catch(err => console.error(err));
 });
